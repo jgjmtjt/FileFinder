d14c5ca77fb14727ad5dfc39f94472fb30efb68b
jgjmtjt <takagikei@takagitoshi-no-MacBook-Air.local>
2015-07-05 19:25:37 +0900

    importの補足
    importの補足

--- src/main/java/com/github/finder/Main.java
+++ src/main/java/com/github/finder/Main.java
@@ -1,5 +1,9 @@
 package com.github.finder;
 
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.CmdLineException;
+
+
 public class Main{
     
     public Main(String[] arguments){
